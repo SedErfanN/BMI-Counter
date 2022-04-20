db4fe6644c9e5a7fc746289aa7fb0348c98ffd6e
SedErfanN <103873262+SedErfanN@users.noreply.github.com>
2022-04-20 10:06:14 +0430

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,54 @@
+function calculate(){
+    //Need to determine the constant of some id functions.
+      var bmi;
+      var result = document.getElementById("result");
+    //The value of the height slider
+      var height = parseInt(document.getElementById("height").value);
+    //The value of the weight slider
+      var weight = parseInt(document.getElementById("weight").value);
+  
+    //The value of height and width should be displayed in the webpage using "textContent".
+      document.getElementById("weight-val").textContent = weight + " kg";
+      document.getElementById("height-val").textContent = height + " cm";
+  
+  //Now I have added the formula for calculating BMI in "bmi"
+      bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+    //With the help of "textContent" we have arranged to display in the result page of BMI
+      result.textContent = bmi;
+  
+  
+    //Now we have to make arrangements to show the text
+  
+  
+    //When the BMI is less than 18.5, you can see the text below
+      if(bmi < 18.5){
+          category = "Underweight";
+          document.body.style.backgroundColor = "#ffc44d";
+          result.style.color = "#ffc44d";
+      }
+  
+    //If BMI is >=18.5 and <=24.9
+      else if( bmi >= 18.5 && bmi <= 24.9 ){
+          category = "Normal Weight";
+          document.body.style.backgroundColor = "rgb(93, 220, 93)";
+          result.style.color = "rgb(93, 220, 93)";
+      }
+  
+    //If BMI is >= 25 and <= 29.9 
+      else if( bmi >= 25 && bmi <= 29.9 ){
+          category = "Overweight";
+          document.body.style.backgroundColor = "#ff884d";
+          result.style.color = "#ff884d";
+      }
+  
+    //If BMI is <= 30
+      else{
+          category = "Obese ";
+          document.body.style.backgroundColor = "red";
+          result.style.color = "red";
+      }
+    //All of the above text is stored in "category".
+  
+  //Now you have to make arrangements to display the information in the webpage with the help of "textContent"
+      document.getElementById("category").textContent = category;
+    }
